c7c6b8ddbc7eed9db91549a468c10f07c2619c3f
galeno <gui.galenocover@gmail.com>
2022-02-01 14:27:23 -0300

    Class Component #02

--- src/components/Saudacao.jsx
+++ src/components/Saudacao.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Saudacao extends Component {
-    state = {
-        tipo: 'Salve',
-        nome: 'Galeno'
-    }
 
+    state = {
+        tipo: this.props.tipo,
+        nome: this.props.tipo
+    }
+    constructor(props) {
+        super(props)
+        this.setTipo = this.setTipo.bind(this)
+        this.setNome = this.setNome.bind(this)
+
+    }
     setTipo(e) {
         this.setState({
             tipo: e.target.value
@@ -24,9 +30,9 @@ export default class Saudacao extends Component {
                 <h1>{tipo} {nome}!</h1>
                 <hr />
                 <input type="text" placeholder='Tipo...' value={tipo}
-                    onChange={e => this.setTipo(e)} />
+                    onChange={this.setTipo} />
                 <input type="text" placeholder='Nome...' value={nome}
-                    onChange={e => this.setNome(e)} />
+                    onChange={this.setNome} />
             </div>
         )
     }
