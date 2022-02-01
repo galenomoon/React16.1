6753fa7277982e1b5c91a921ecdb22263ba0a9b6
galeno <gui.galenocover@gmail.com>
2022-02-01 14:01:52 -0300

    Start Point| Props and Components

---
+++ src/components/BomDia.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Primeiro from "./Primeiro";
+
+export default props =>
+<div>
+    <h1>Bom dia, {props.name}</h1>
+    <Primeiro/>
+</div>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BomDia from "./components/BomDia";
+
+ReactDOM.render(
+  //JSX
+  <div>
+    <BomDia name="Galeno" />
+  </div>,
+  document.getElementById("root")
+);
