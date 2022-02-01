7aaf16a06afe89db6c0275b92781546c66fba1ad
galeno <gui.galenocover@gmail.com>
2022-02-01 14:22:58 -0300

    Class Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Saudacao from "./components/Saudacao";
 // import BomDia from "./components/BomDia";
-import {BoaTarde, BoaNoite} from './components/Multiplos'
+// import {BoaTarde, BoaNoite} from './components/Multiplos'
+
 
 ReactDOM.render(
   //JSX
   <div>
+      <Saudacao tipo="Bom dia" nome="joão"/>
     {/* <BomDia name="Galeno" /> */}
-    <BoaTarde name="Gusta" />
-    <BoaNoite name="Luana" />
+    {/* <BoaTarde name="Gusta" /> */}
+    {/* <BoaNoite name="Luana" /> */}
   </div>,
   document.getElementById("root")
 );
